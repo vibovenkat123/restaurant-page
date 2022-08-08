8cbea992860d70f9083ddb40a8609bfbee27deca
vibovenkat123 <vibovenkat@gmail.com>
2022-08-08 17:18:52 -0500

    finsihed setting up the basics of the grid for the text

--- src/menu.js
+++ src/menu.js
@@ -8,6 +8,18 @@ export function createMenu(){
 function createContainer(menu){
   const gridContainer = document.createElement('div');
   gridContainer.classList.add('grid-container');
+  createContainerItems(gridContainer);
   menu.appendChild(gridContainer)
+}
+function createContainerItems(container){
+  const iceCreamArray = ['Strawberry (New !!)', 'Cherry Vanilla', 'Peanut Butter Cup', 'Coffee', 'Chocolate', 'Rocky Road', 'Chocolate Almond', 'Lemon', 'Chocolate Chip Cookie Dough'];
+  for (const iceCream in iceCreamArray){
+    let gridElement = document.createElement('div');
+    gridElement.classList.add('grid-element')
+    let heading = document.createElement('h1');
+    heading.textContent = iceCreamArray[iceCream];
+    gridElement.appendChild(heading);
+    container.appendChild(gridElement);
+  }
 }
 export {menuDiv}
