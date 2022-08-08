7bbe4e007b9426c2be9c5f9c6d05dadad54a977d
vibovenkat123 <vibovenkat@gmail.com>
2022-08-08 17:57:41 -0500

    finished grid changed grid background color to a background image

--- src/menu.js
+++ src/menu.js
@@ -3,6 +3,7 @@ export function createMenu(){
   menuDiv = document.createElement('div');
   menuDiv.classList.add('menu');
   createContainer(menuDiv)
+  addCredit(menuDiv)
   return menuDiv;
 }
 function createContainer(menu){
@@ -18,8 +19,17 @@ function createContainerItems(container){
     gridElement.classList.add('grid-element')
     let heading = document.createElement('h1');
     heading.textContent = iceCreamArray[iceCream];
+    let paragraph = document.createElement('p');
+    paragraph.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit?'
     gridElement.appendChild(heading);
+    gridElement.appendChild(paragraph);
     container.appendChild(gridElement);
   }
+}
+function addCredit(menu){
+  const creditDiv = document.createElement('div');
+  creditDiv.classList.add('credits');
+  creditDiv.innerHTML = 'Photo by <a href="https://unsplash.com/@slashiophotography?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Slashio Photography</a> on <a href="https://unsplash.com/s/photos/ice-cream?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>';
+  menu.appendChild(creditDiv);
 }
 export {menuDiv}
