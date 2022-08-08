e3622b550e848464b7dbc07fd73d52f220787410
vibovenkat123 <vibovenkat@gmail.com>
2022-08-08 18:43:43 -0500

    add the basic outline of the box on the contact page

--- src/contact.js
+++ src/contact.js
@@ -2,9 +2,12 @@ let contactDiv;
 export function createContactDiv(){
   contactDiv = document.createElement('div');
   contactDiv.classList.add('contact');
+  createInfoBox(contactDiv);
   return contactDiv;
 }
-function createInfoBox(){
-  const infoBox = document.createElement('div') 
+function createInfoBox(div){
+  const infoBox = document.createElement('div');
+  infoBox.classList.add('info-box');
+  div.appendChild(infoBox); 
 }
 export {contactDiv}
